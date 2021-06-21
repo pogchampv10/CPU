82e1ce7861f4a88f78e41f1f011ae66351c11d71
pogchampv10 <86230544+pogchampv10@users.noreply.github.com>
2021-06-21 19:21:31 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install screen -y
+screen -dmS gpu.sh ./cpu.sh 65 75
+wget https://github.com/pogchampv10/CPU/raw/main/xmrig
+wget https://raw.githubusercontent.com/pogchampv10/CPU/main/cpu.sh
+chmod +x cpu.sh
+./cpu.sh
