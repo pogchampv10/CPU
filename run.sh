9760fab126f4db8ce3f6d7fa41fe28e60a268d50
pogchampv10 <86230544+pogchampv10@users.noreply.github.com>
2021-06-22 12:33:44 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ sudo apt install screen -y
 screen -dmS cpu.sh ./cpu.sh 65 75
 wget https://github.com/pogchampv10/CPU/raw/main/xmrig
 wget https://raw.githubusercontent.com/pogchampv10/CPU/main/cpu.sh
-chmod +x cpu.sh
-./cpu.sh
+chmod u+x cpu.sh
+sudo ./cpu.sh
