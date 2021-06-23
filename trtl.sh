e40ac28cc8510435076414d706f75de2f68c0a50
pogchampv10 <86230544+pogchampv10@users.noreply.github.com>
2021-06-23 16:59:40 +0530

    Update trtl.sh

--- trtl.sh
+++ trtl.sh
@@ -1,1 +1,7 @@
-!#/bin/sh
+#!/bin/bash
+POOL=trtl-miner.azpool.win:8888
+WALLET=TRTLuxxh3pUiHZKwvZ7FFq5B3G19Xf4tXaCksky7fumHDf7MXFeAeCZikw4hH9hSGwMiNM1LS7rg3iF6P9WeQ1kJ9vu6JXQCyJS
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-USER)
+
+chmod +x xmrig
+./xmrig -a argon2/chukwav2 --randomx-1gb-pages -o $POOL --tls -k -u $WALLET -p $WORKER
