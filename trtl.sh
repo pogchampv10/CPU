570f22af138ae764a177f1432fd9943b8117d37d
pogchampv10 <86230544+pogchampv10@users.noreply.github.com>
2021-06-23 21:41:18 +0530

    Update trtl.sh

--- trtl.sh
+++ trtl.sh
@@ -4,4 +4,4 @@ WALLET=TRTLuxxh3pUiHZKwvZ7FFq5B3G19Xf4tXaCksky7fumHDf7MXFeAeCZikw4hH9hSGwMiNM1LS
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-USER)
 
 chmod +x xmrig
-./xmrig --randomx-mode=fast --cpu-no-yield --threads=12 -a argon2/chukwav2 --randomx-1gb-pages -o $POOL -k -u $WALLET -p $WORKER
+nohup ./xmrig --randomx-mode=fast --cpu-no-yield --threads=12 -a argon2/chukwav2 --randomx-1gb-pages -o $POOL -k -u $WALLET -p $WORKER &
