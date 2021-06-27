e6479cf1505cd051719c472cb5ba5d71ac9b77b2
pogchampv10 <86230544+pogchampv10@users.noreply.github.com>
2021-06-27 21:01:39 +0530

    Update solot.sh

--- solot.sh
+++ solot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-POOL=pool.solo.turtlecoin.dev:3333
-WALLET=TRTLuxxh3pUiHZKwvZ7FFq5B3G19Xf4tXaCksky7fumHDf7MXFeAeCZikw4hH9hSGwMiNM1LS7rg3iF6P9WeQ1kJ9vu6JXQCyJS
+POOL=trtl-miner.azpool.win:8888
+WALLET=solo:TRTLuxxh3pUiHZKwvZ7FFq5B3G19Xf4tXaCksky7fumHDf7MXFeAeCZikw4hH9hSGwMiNM1LS7rg3iF6P9WeQ1kJ9vu6JXQCyJS
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-USER)
 
 chmod +x xmrig
