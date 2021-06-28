aa48f7e23fe14fab1407ff6b00e668702f096a69
pogchampv10 <86230544+pogchampv10@users.noreply.github.com>
2021-06-28 08:35:59 +0530

    Update trtl.sh

--- trtl.sh
+++ trtl.sh
@@ -4,4 +4,4 @@ WALLET=TRTLuxxh3pUiHZKwvZ7FFq5B3G19Xf4tXaCksky7fumHDf7MXFeAeCZikw4hH9hSGwMiNM1LS
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-USER)
 
 chmod +x xmrig
-./xmrig --randomx-mode=fast --randomx--1gb-pages --cpu-no-yield --donate-level 0 -o $POOL -u $WALLET -p $WORKER -a argon2/chukwav2 -k --tls
+./xmrig --randomx-mode=fast --randomx--1gb-pages --tls --cpu-no-yield --donate-level 0 -o $POOL -u $WALLET -p $WORKER -a argon2/chukwav2 -k
