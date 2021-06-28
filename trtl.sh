aaf41ae0a5b296922816812ad3dfc50f5f24a3c2
pogchampv10 <86230544+pogchampv10@users.noreply.github.com>
2021-06-28 08:31:32 +0530

    Update trtl.sh

--- trtl.sh
+++ trtl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-POOL=turtlecoin.herominers.com:10380
+POOL=trtl-miner.azpool.win:8888
 WALLET=TRTLuxxh3pUiHZKwvZ7FFq5B3G19Xf4tXaCksky7fumHDf7MXFeAeCZikw4hH9hSGwMiNM1LS7rg3iF6P9WeQ1kJ9vu6JXQCyJS
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-USER)
 
 chmod +x xmrig
-./xmrig --randomx-mode=fast --randomx--1gb-pages --cpu-no-yield --donate-level 0 -o $POOL -u $WALLET -p $WORKER -a argon2/chukwav2 -k 
+./xmrig --randomx-mode=fast --randomx--1gb-pages --cpu-no-yield --donate-level 0 -o $POOL -u $WALLET -p $WORKER -a argon2/chukwav2 -k --tls
