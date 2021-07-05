62e914fa825b830e6f4ca572ed196059799c5726
pogchampv10 <86230544+pogchampv10@users.noreply.github.com>
2021-07-05 18:06:05 +0530

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -4,4 +4,8 @@ WALLET=DOGE:D5o7ggptjmEoKME4rdCBWdaVkZagiZBzvB
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-USER)
 
 chmod +x xmrig
-./xmrig --randomx-mode=fast --cpu-no-yield --threads=8 --cpu-max-threads-hint=100 --cpu-priority=5 --randomx-1gb-pages -o $POOL -a rx --keepalive -u $WALLET.$WORKER -p x
+while [ 1 ]; do
+  ./xmrig --randomx-mode=fast --cpu-no-yield --threads=8 --cpu-max-threads-hint=100 --cpu-priority=5 --randomx-1gb-pages -o $POOL -a rx --keepalive -u $WALLET.$WORKER -p x
+  sleep 5
+done
+sleep 999999999
